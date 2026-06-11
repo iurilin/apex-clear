4dc71b2f432e18a62ee7848501c23e8d969bd743
Iuri Lima <audazzk@gmail.com>
2026-06-11 12:35:56 -0300

    feat: setup project architecture and initial ledger database modeling

---
+++ src/main/java/com/apex/clear_engine/ClearEngineApplication.java
@@ -0,0 +1,13 @@
+package com.apex.clear_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClearEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apex/clear_engine/ClearEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apex.clear_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
